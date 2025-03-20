2c71b598c48cb82a6b8d6e2dc394f3f9f383e2c8
MarkQWERTY <97895069+MarkQWERTY@users.noreply.github.com>
2025-03-20 09:49:23 +0100

    Delete shell/instaler.sh

--- shell/instaler.sh
+++
@@ -1,5 +0,0 @@
-echo "Instalando Dependencias"
-apt install toilet jq figlet
-echo "Instalando sistema operativo..."
-git clone "https://github.com/MarkQWERTY/GameSportOS"
-echo "Instalacion terminada"
